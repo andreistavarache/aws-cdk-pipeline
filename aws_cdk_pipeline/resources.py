2694e178d9be514ab18ab7ed9f62c60eb6348488
andrei.stavarache <stavaracheandrei92@gmail.com>
2023-05-25 16:57:21 +0300

    a

--- aws_cdk_pipeline/resources.py
+++ aws_cdk_pipeline/resources.py
@@ -15,10 +15,14 @@ class ResourceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # queue = sqs.Queue(
-        #     self, "AwsCdkPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-        # bucket = s3.Bucket(self, "MyfirstBucket", 
-        #                    versioned=True,
-        #                    block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        queue = sqs.Queue(
+            self, "AwsCdkPipelineQueue",
+            visibility_timeout=Duration.seconds(300),
+        )
+        bucket = s3.Bucket(self, "MyfirstBucket", 
+                           versioned=True,
+                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        queue2 = sqs.Queue(
+            self, "AwsCdkPipelineQueue2",
+            visibility_timeout=Duration.seconds(300),
+        )
